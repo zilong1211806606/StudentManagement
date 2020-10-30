a3b3cef9733f48a3377a65816016bded92b49b32
1211806606@qq.com <1211806606@qq.com>
2020-10-30 13:48:45 +0800

    没做什么事情

--- StudentManagement/StudentManagement/Startup.cs
+++ StudentManagement/StudentManagement/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +13,13 @@ namespace StudentManagement
 {
     public class Startup
     {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -33,7 +41,7 @@ namespace StudentManagement
                 endpoints.MapGet("/", async context =>
                 {
                     string processName = System.Diagnostics.Process.GetCurrentProcess().ProcessName; ;
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(_configuration["MyKey"]);
                 });
             });
         }
